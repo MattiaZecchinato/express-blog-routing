47512eaf5eecb9ed46285573979f56ee4da86c3c
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-27 13:40:11 +0100

    feat: add posts router

---
+++ routers/posts.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+// index
+router.get('/', (req, res) => {
+
+    console.log('index');
+    res.send('Posts list'); 
+});
+
+// show
+router.get('/:id', (req, res) => {
+
+    console.log('show');
+    res.send(`Details of post ${req.params.id}`); 
+});
+
+// store 
+router.post('/', (req, res) => {
+
+    console.log('store');
+    res.send('Create new post'); 
+});
+
+// update 
+router.put('/:id', (req, res) => {
+
+    console.log('update');
+    res.send(`Full post edit of ${req.params.id}`);
+}); 
+
+// modify 
+router.patch('/:id', (req, res) => {
+
+    console.log('modify');
+    res.send(`Partial post edit of ${req.params.id}`); 
+}); 
+
+// delete 
+router.delete('/:id', (req, res) => {
+
+    console.log('delete');
+    res.send(`Deleting post ${req.params.id}`);
+});
+
+module.exports = router;
