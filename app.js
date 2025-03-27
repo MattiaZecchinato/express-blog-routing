26cb989bde0ae3349bee6db8cbf4ea1b5a3d1ce8
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-27 13:40:40 +0100

    feat: add posts router to app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+const postsRouter = require('./routers/posts.js');
+
+app.get('/', (req, res) => {
+
+    console.log('base test');
+    res.send('base test');
+})
+
+app.use("/posts", postsRouter);
+
+app.listen(port, () => {
+
+    console.log('listen');
+})
